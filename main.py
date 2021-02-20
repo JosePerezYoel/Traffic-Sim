a67a77a6997ca6dec1741ce90482904385988ab1
Jose <joseperezyoel2003@gmail.com>
2021-02-20 18:21:38 -0500

    Started

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+
+pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("Traffic Simulation")
+
+running = True
+
+while running:
+    pass
